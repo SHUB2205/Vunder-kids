d4fedefad6759b9c0810f49c6b5c9b8302effb78
ParasY1724 <parasyerunkar174@gmail.com>
2024-07-25 00:04:45 +0530

    Added Validation

--- vunder-kids/backend/routes/userRoutes.js
+++ vunder-kids/backend/routes/userRoutes.js
@@ -1,10 +1,42 @@
 const express = require('express');
 const { registerUser, loginUser,verifyEmail,sendVerificationEmail } = require('../controllers/userController');
 const router = express.Router();
+const { check, body } = require('express-validator');
 
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
+
+router.post('/register',
+    [
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email.')
+      .normalizeEmail(),
+    body(
+      'password',
+      'Please enter a password of at least 5 characters.'
+    )
+      .isLength({ min: 5 })
+      .trim(),
+    body('confirmPassword') //this field must be in frontend 
+      .trim()
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('Passwords have to match!');
+        }
+        return true;
+      })
+  ], registerUser);
+
+router.post('/login', [
+    body('email')
+      .isEmail()
+      .withMessage('Please enter a valid email address.')
+      .normalizeEmail(),
+    body('password', 'Password has to be valid.')
+      .isLength({ min: 5 })
+      .trim()
+  ],loginUser);
+
 router.post('/send-verification-email',sendVerificationEmail);
 router.get('/verify-email/:token', verifyEmail);
 
