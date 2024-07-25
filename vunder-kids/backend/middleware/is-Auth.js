135cdebca09c6589dfe4771436bb4a3106332e49
vinki100z <vikrantchauhancf2021@gmail.com>
2024-07-25 23:16:46 +0530

    Changes in reset Passwords

--- vunder-kids/backend/middleware/is-Auth.js
+++ vunder-kids/backend/middleware/is-Auth.js
@@ -2,14 +2,15 @@ var jwt = require("jsonwebtoken");
 
 const isAuth=async (req,res,next)=>{
     try {
-        let token=req.header("auth-token")
+        let token=req.header("token")
         if(!token){
             return res.send("Token Is Not Correct");
         }
         const data = jwt.verify(token,process.env.JWT_SECRET);
-        // console.log(data);
+
         // { user: { id: '64b0873ba5029152669bd531' }, iat: 1689290555 }
-        req.user = data.user;
+        req.user = data;
+      
         next();
     } catch (error) {
         res.status(400).json({ error: error.message });
--- vunder-kids/backend/routes/userRoutes.js
+++ vunder-kids/backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { registerUser, loginUser ,requestResetPassword , resetPassword } = require('../controllers/userController');
+const { registerUser, loginUser ,requestResetPassword , resetPassword ,userId} = require('../controllers/userController');
 const router = express.Router();
 
 
@@ -10,6 +10,7 @@ router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post("/request-reset-password",isAuth,requestResetPassword);
 router.post("/reset-password/:token",resetPassword);
+router.get("/userId",isAuth,userId);
 
 
 module.exports = router;
