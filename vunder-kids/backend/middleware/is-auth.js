1bcdde4ddbb6de1b3c1f82c43ef86e6baceee7f1
vinki100z <vikrantchauhancf2021@gmail.com>
2024-07-26 10:36:12 +0530

    ch

--- vunder-kids/backend/middleware/is-auth.js
+++ vunder-kids/backend/middleware/is-auth.js
@@ -1,24 +1,19 @@
-const jwt = require('jsonwebtoken');
+var jwt = require("jsonwebtoken");
 
-module.exports = async (req, res, next) => {
-  try {
-    const token = req.get('Authorization')?.split(' ')[1];
-    if (!token) {
-      const error = new Error('Not authenticated.');
-      error.statusCode = 401;
-      throw error;
+const isAuth=async (req,res,next)=>{
+    try {
+        let token=req.header("token")
+        if(!token){
+            return res.send("Token Is Not Correct");
+        }
+        const data = jwt.verify(token,process.env.JWT_SECRET);
+
+        // { user: { id: '64b0873ba5029152669bd531' }, iat: 1689290555 }
+        req.user = data;
+      
+        next();
+    } catch (error) {
+        res.status(400).json({ error: error.message });
     }
-    const decodedToken = jwt.verify(your_jwt_secret);
-    if (!decodedToken) {
-      const error = new Error('Not authenticated.');
-      error.statusCode = 401;
-      throw error;
-    }
-    
-    req.userId = decodedToken.id;
-    next();
-  }
-  catch (err) {
-    console.log(err.message);
-  }
-};
+}
+module.exports=isAuth;
