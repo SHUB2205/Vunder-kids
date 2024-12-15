f0f3d52d8e638284a9e00828f44c6d9193d7791c
ParasY1724 <parasyerunkar174@gmail.com>
2024-12-15 10:37:42 +0530

    PWA Setup

--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-     "node_modules/react-tooltip/dist/react-tooltip.css"
+  content: ["./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/react-tooltip/dist/react-tooltip.css"
   ],
+  important: "#root",
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
