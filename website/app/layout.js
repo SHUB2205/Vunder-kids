7560c483ebe0aac9d18de049abab2645b4e278c7
SHUB2205 <parasharshubhankar22@gmail.com>
2026-05-01 15:05:05 -0400

    analytics

--- website/app/layout.js
+++ website/app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css'
+import { Analytics } from '@vercel/analytics/next'
 
 export const metadata = {
   metadataBase: new URL('https://fisiko.io'),
@@ -16,7 +17,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
